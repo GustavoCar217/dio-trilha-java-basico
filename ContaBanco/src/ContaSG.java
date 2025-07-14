ed4a780bfa5f4e475b13a91e1de6be397acf3953
GustavoCar217 <GustavoVieira217@hotmail.com>
2025-07-14 19:46:13 -0300

    finalização do projeto conta banco

---
+++ ContaBanco/src/ContaSG.java
@@ -0,0 +1,31 @@
+public class ContaSG {
+    private String agencia;
+    private int numero;
+    private String nome;
+    private float saldo;
+
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+}
--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ContaSG conta = new ContaSG();
+        conta.setAgencia("075-9");
+        conta.setNumero(2065);
+        conta.setNome("João Silva");
+        conta.setSaldo(650.75f);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao Banco!");
+        System.out.println("Digite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        if ( agencia.equals(conta.getAgencia()) ) {
+            System.out.println("Digite o número da conta: ");
+            System.out.print("Conta criada com sucesso! ");
+            int numero = scanner.nextInt();
+            if ( numero == conta.getNumero() ) {
+                System.out.println("Olá " + conta.getNome() + " Obrigado por criar uma conta em nosso banco, sua agência é "
+                 + conta.getAgencia() + ", conta número " + conta.getNumero() +
+                " e seu saldo " + conta.getSaldo() + " já está disponível para saque.");
+            } else {
+                System.out.println("Número da conta incorreto.");
+            }
+        } else {
+            System.out.println("Agência incorreta.");
+        }
+        
     }
 }
